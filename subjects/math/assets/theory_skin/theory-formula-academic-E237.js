9dd45b3208e5502eaa5d8a04ebb15ef0a762ec6d
BlueDragon33 <dinhnam331007@gmail.com>
2026-07-07 19:02:47 +0700

    E237A fix HTML escaping in academic bridge

--- subjects/math/assets/theory_skin/theory-formula-academic-E237.js
+++ subjects/math/assets/theory_skin/theory-formula-academic-E237.js
@@ -10,7 +10,7 @@
 
   function esc(s){
     return String(s==null?'':s).replace(/[&<>"']/g,function(c){
-      return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'}[c];
+      return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c];
     });
   }
 
