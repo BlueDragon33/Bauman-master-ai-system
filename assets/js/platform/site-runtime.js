89d02d2d4f9d534826431f43c9f971ab56fd3f9e
BlueDragon33 <dinhnam331007@gmail.com>
2026-08-24 14:23:11 +0700

    chore(l5): bump site runtime for diagnostics shell

--- assets/js/platform/site-runtime.js
+++ assets/js/platform/site-runtime.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const config=global.BAUMAN_RUNTIME_CONFIG||{};
-  const VERSION='2026.08.24-l5.9';
+  const VERSION='2026.08.24-l5.10';
   const SCRIPT_URL=global.document?.currentScript?.src||'';
 
   function runtimeBase(){
